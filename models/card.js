2d41865884578485081f4abb3df1205260ef140a
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-10-18 19:03:07 -0400

    Change card object

--- models/card.js
+++ models/card.js
@@ -6,8 +6,24 @@ const Schema = mongoose.Schema;
 // USER MODEL
 const CardSchema = new Schema(
   {
-    id: String,
-    count: Number
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    supertype: { type: String, required: true },
+    subtypes: [{ type: String }],
+    level: { type: String, default: null },
+    set: {
+      name: { type: String, required: true },
+      series: { type: String, default: null },
+      totalPrint: { type: Number, required: true }
+    },
+    number: { type: String, required: true },
+    rarity: { type: String, required: true },
+    images: {
+      small: { type: String, required: true },
+      large: { type: String, required: true }
+    },
+    marketValue: { type: Number, required: true }
+    // count: Number
   },
   { timestamps: true }
 );
