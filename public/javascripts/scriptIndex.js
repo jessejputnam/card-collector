e48c29764a2c7ffd7a7b36d29c637fa9cccec9ba
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-11-05 15:14:41 -0400

    Update button and link aesthetic

---
+++ public/javascripts/scriptIndex.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const slides = document.querySelectorAll(".slide");
+const firstSlide = document.querySelector(".first-slide");
+
+let index = 0;
+slides[index].classList.add("active");
+firstSlide.classList.add("inactive");
+
+setInterval(() => {
+  slides[index].classList.remove("active");
+
+  index++;
+
+  if (index === slides.length) index = 0;
+
+  slides[index].classList.add("active");
+}, 3500);
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,10 @@ const express = require("express");
 const router = express.Router();
 
 const auth_controller = require("../controllers/authController");
-const card_controller = require("../controllers/cardController");
 
 const authCheckTrue = require("../helpers/authCheckTrue");
 
-router.get("/", authCheckTrue, (req, res) => {
-  res.render("index", { title: "Card Collector" });
-});
+router.get("/", authCheckTrue, auth_controller.index_get);
 
 router.get("/register", authCheckTrue, auth_controller.signup_get);
 router.post("/register", auth_controller.sign_up_post);
