b2cd8b4c2930c832a24ec711f802b71d9e4fb499
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-11-06 16:36:30 -0500

    Make mobile friendly: card detail, collection, search, binders, forms

--- public/javascripts/scriptNav.js
+++ public/javascripts/scriptNav.js
@@ -1,4 +1,30 @@
 "use strict";
 
+const topbar = document.querySelector(".topbar");
 const collectionBtn = document.querySelector("#collection-menu-btn");
+const collectionMenu = document.querySelector(".collection-menu");
 const bulkBtn = document.querySelector("#bulk-menu-btn");
+const bulkMenu = document.querySelector(".bulk-menu");
+
+if (collectionBtn) {
+  collectionBtn.addEventListener("click", (e) => {
+    collectionMenu.classList.toggle("menu-open");
+  });
+}
+
+if (bulkBtn) {
+  bulkBtn.addEventListener("click", (e) => {
+    bulkMenu.classList.toggle("menu-open");
+  });
+}
+
+if (topbar) {
+  document.addEventListener("click", (e) => {
+    if (e.target.id !== "collection-menu-btn") {
+      collectionMenu.classList.remove("menu-open");
+    }
+    if (e.target.id !== "bulk-menu-btn") {
+      bulkMenu.classList.remove("menu-open");
+    }
+  });
+}
