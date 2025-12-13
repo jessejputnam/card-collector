a0114317338b53565def5f5ba7562a5811ecac96
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2025-12-13 14:50:15 -0500

    Add set detail page; update set symbol url

--- controllers/setController.js
+++ controllers/setController.js
@@ -1,25 +1,85 @@
-const Card = require("../models/card");
 const CardSet = require("../models/set");
 const handle = require("../helpers/errorHandler");
-const sort = require("../helpers/sort");
-const makeCSV = require("../helpers/makeCSV");
-const getConversionRate = require("../helpers/getConversionRate");
 
 // Handle display cards by set on GET
 exports.display_sets_get = async (req, res, next) => {
   const userId = req.user._id;
   const curr = req.user.curr;
 
+  const isAdmin = userId == process.env.ADMIN_USER_ID;
+
+  const fields =
+    "id name series tcgPlayerId releaseDate cardCount symbolUrl priceGuideUrl imageUrl";
+
   const [setsErr, sets] = await handle(
-    CardSet.find({}, "name tcgPlayerId releaseDate cardCount")
-      .sort({ releaseDate: -1 })
-      .exec()
+    CardSet.find({}, fields).sort({ releaseDate: -1 }).exec()
   );
   if (setsErr) return next(setsErr);
 
   return res.render("sets", {
     title: "Card Sets",
     setList: sets,
+    isAdmin,
     curr
   });
+};
+
+// Handle display individual set detail on GET
+exports.display_set_detail_get = async (req, res, next) => {
+  const userId = req.user._id;
+  const curr = req.user.curr;
+
+  const isAdmin = userId == process.env.ADMIN_USER_ID;
+
+  const setId = req.params.setId;
+
+  const fields =
+    "id name series tcgPlayerId releaseDate cardCount symbolUrl priceGuideUrl imageUrl";
+
+  const [setErr, set] = await handle(
+    CardSet.findOne({ id: setId }, fields).exec()
+  );
+  if (setErr) return next(setErr);
+  if (!set) {
+    const err = new Error("Set not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  return res.render("set-detail", {
+    title: `Set Detail`,
+    set,
+    isAdmin,
+    curr
+  });
+};
+
+// Handle update set on POST (admin only)
+exports.update_set_post = async (req, res, next) => {
+  const curr = req.user.curr;
+  const isAdmin = req.user._id == process.env.ADMIN_USER_ID;
+  if (!isAdmin) {
+    const err = new Error("Unauthorized");
+    err.status = 401;
+    return next(err);
+  }
+
+  const setId = req.params.setId;
+  const updateData = req.body;
+  console.log(updateData);
+
+  const [updateErr, updatedSet] = await handle(
+    CardSet.findOneAndUpdate(
+      { id: setId },
+      { $set: { symbolUrl: updateData.symbolUrl } }
+    ).exec()
+  );
+  if (updateErr) return next(updateErr);
+  if (!updatedSet) {
+    const err = new Error("Set not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  return res.redirect(`/sets/${setId}`);
 };
--- routes/sets.js
+++ routes/sets.js
@@ -10,4 +10,12 @@ const set_controller = require("../controllers/setController");
 // GET request for collection by set
 router.get("/", set_controller.display_sets_get);
 
+// GET request for individual set detail
+router.get("/:setId", set_controller.display_set_detail_get);
+
+// POST request to update set (admin only)
+router.post("/:setId", set_controller.update_set_post);
+
+// ###########################################
+
 module.exports = router;
